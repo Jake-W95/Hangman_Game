d7ad23d3d8182971121cdfd04d32a01d1df6e8b7
PaulKnees <jakewallace368@gmail.com>
2022-12-18 13:54:46 +0000

    lives system, imperfect

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,37 +2,56 @@ var newWordBtn = $('#newWord');
 var wordSec = $('#wordSec')
 
 // console.log(letter[0].innerText)
+var lives = 5
+var livesLeft = $('#livesLeft')
 
 
+// document.ready(setRemainingLives())
+
+
+
+
+
+function setRemainingLives() {
+    
+    $(livesLeft).text(lives)
+}
 $(newWordBtn).click(function () {
     var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
     $(wordSec).empty();
+    setRemainingLives()
 
     for (var C of randomWord) {
         $(wordSec).append(`
-    <div class="letter">${C}</div>
-    `)
+        <div class="letter">${C}</div>
+        `)
         var letters = $('.letter');
-        
-        
+        // console.log(letters)
+
+
         $(document).keydown(function (event) {
             var key = event.originalEvent.key;
-            
             for (var L of letters) {
                 if (L.innerText === key.toUpperCase()) {
                     $(L).addClass('correct');
-                } 
+                }
+            }
+            if (randomWord.indexOf(key) === -1) {
+                alert('inc')
+                lives--;
+                setRemainingLives()
             }
         })
 
-        }
-        if($(letters).hasClass('correct')){
-            alert('win')}
+    }
+    if ($(letters).hasClass('correct')) {
+        alert('win')
+    }
 
-        
-    
-    
-    
+
+
+
+
 })
 // console.log(wordList[0][0])
 
