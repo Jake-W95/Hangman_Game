1651f80322f8dfd688b56de72717f194ce366c92
PaulKnees <jakewallace368@gmail.com>
2022-12-18 13:02:41 +0000

    changes layout

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -5,29 +5,34 @@ var wordSec = $('#wordSec')
 
 
 $(newWordBtn).click(function () {
-var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
-$(wordSec).empty();
+    var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+    $(wordSec).empty();
 
-  for(var C of randomWord){
-    $(wordSec).append(`
+    for (var C of randomWord) {
+        $(wordSec).append(`
     <div class="letter">${C}</div>
     `)
-var letter = $('.letter');
-
-    
-    $(document).keydown(function (event) {
-        var key = event.originalEvent.key;
+        var letters = $('.letter');
         
-        for (var L of letter) {
-            if (L.innerText === key.toUpperCase()) {
-                $(L).addClass('correct');
+        
+        $(document).keydown(function (event) {
+            var key = event.originalEvent.key;
+            
+            for (var L of letters) {
+                if (L.innerText === key.toUpperCase()) {
+                    $(L).addClass('correct');
+                } 
             }
+        })
+
         }
-    })
-  }
+        if($(letters).hasClass('correct')){
+            alert('win')}
 
-  console.log(letter)
-
+        
+    
+    
+    
 })
 // console.log(wordList[0][0])
 
